cb3504675b028fa4dfbbee528f7bd2877ad8f3e8
V1niSouza <viniciussouzaramospv@gmail.com>
2025-03-10 01:05:26 -0300

    Feat: Adicionando rota para adição em Lista

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,7 +1,24 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+# Essa biblioteca serve para ler uma determinada URL
+import urllib
+# Converte dados para o formato json
+import json
+
+gatos = []
+
 
 def init_app(app):
     @app.route('/')
     # View function -> função de visualização
     def home():
         return render_template('index.html')
+
+    @app.route('/catlists', methods=['GET', 'POST'])
+    def catlists():
+        if request.method == 'POST':
+            if request.form.get('gato'):
+                gatos.append(request.form.get('gato'))
+                return redirect(url_for('catlists'))
+
+        return render_template('catlist.html',
+                               gatos=gatos)
