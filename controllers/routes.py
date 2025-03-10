6f11b0ef11f438487dc5b61b43c1d1f5d6aa5d15
V1niSouza <viniciussouzaramospv@gmail.com>
2025-03-10 01:26:48 -0300

    Feat: Adicionando rota para consumir API publica

--- controllers/routes.py
+++ controllers/routes.py
@@ -33,3 +33,29 @@ def init_app(app):
             gatolist.append(form_data)
             return (redirect(url_for('catDictionary')))
         return render_template('catDictionary.html', gatolist=gatolist)
+    
+
+    @app.route('/apigatos', methods=['GET', 'POST'])
+    # Passando parâmetros para a rota
+    @app.route('/apigatos/<string:id>', methods=['GET', 'POST'])
+    # Definindo que o parâmetro é opcional
+    def apigatos(id=None):
+        url = 'https://api.thecatapi.com/v1/breeds'
+        res = urllib.request.urlopen(url)
+        # print(res)
+        data = res.read()
+        gatosjson = json.loads(data)
+
+        if id:
+            ginfo = []
+            for g in gatosjson:
+                if g['id'] == id:
+                    ginfo = g
+                    break
+            if ginfo:
+                return render_template('gatoinfo.html', ginfo=ginfo)
+            else:
+                return f'Gato com a ID {id} não foi encontrado.'
+
+        return render_template('apigatos.html',
+                               gatosjson=gatosjson)
